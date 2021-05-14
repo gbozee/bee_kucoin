29e7139d682eb5452669b7ac834193b21feb0a49
Biola <gbozee@example.com>
2021-05-14 14:32:25 +0100

    added demo code

--- bee_kucoin/__init__.py
+++ bee_kucoin/__init__.py
@@ -1,6 +1,6 @@
+import os
 from kucoin import client as spot_client
 from kucoin_futures import client as future_client
-
 
 class KucoinExchange:
     def __init__(
@@ -76,3 +76,20 @@ class Client:
             passphrase=self.future_passphrase,
             kind="future",
         )
+
+SPOT_API_KEY = os.getenv("KUCOIN_SPOT_API_KEY")
+SPOT_SECRET_KEY = os.getenv("KUCOIN_SPOT_SECRET_KEY")
+SPOT_PASSPHRASE = os.getenv("KUCOIN_PASSPHRASE")
+FUTURE_API_KEY = os.getenv("KUCOIN_FUTURE_API_KEY")
+FUTURE_API_SECRET = os.getenv("KUCOIN_FUTURE_API_SECRET")
+FUTURE_PASSPHRASE = os.getenv("KUCOIN_FUTURE_PASSPHRASE")
+
+
+client = Client(
+    spot_api_key=SPOT_API_KEY,
+    spot_api_secret=SPOT_SECRET_KEY,
+    spot_passphrase=SPOT_PASSPHRASE,
+    future_api_key=FUTURE_API_KEY,
+    future_api_secret=FUTURE_API_SECRET,
+    future_passphrase=FUTURE_PASSPHRASE,
+)
